4f29ec250a079527fc64fa01cfbc2113dd307f12
zjun <565061763@qq.com>
2018-01-16 10:35:08 +0800

    首次提交

---
+++ app/src/main/java/com/fanwe/pageview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.fanwe.pageview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    private FPageView view_page;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view_page = findViewById(R.id.view_page);
+    }
+}
