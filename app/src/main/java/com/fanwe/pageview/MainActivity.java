d1018471848c68b0b1c86144ce7eb84235bbb608
zjun <565061763@qq.com>
2018-01-21 15:21:33 +0800

    重构.

--- app/src/main/java/com/fanwe/pageview/MainActivity.java
+++ app/src/main/java/com/fanwe/pageview/MainActivity.java
@@ -6,16 +6,12 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.fanwe.lib.looper.FLooper;
-import com.fanwe.lib.looper.impl.FSimpleLooper;
 import com.fanwe.lib.pagelayout.FPageLayout;
 
 public class MainActivity extends AppCompatActivity
 {
     private FPageLayout view_page;
     private TextView tv_center;
-
-    private FLooper mLooper = new FSimpleLooper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,22 +29,5 @@ public class MainActivity extends AppCompatActivity
                 Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
             }
         });
-        view_page.setPageView(tv_center);
-
-        mLooper.start(1000, new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                view_page.requestLayout();
-            }
-        });
-    }
-
-    @Override
-    protected void onDestroy()
-    {
-        super.onDestroy();
-        mLooper.stop();
     }
 }
